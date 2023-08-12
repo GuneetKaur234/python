af79ea6160fd572e96ea4b7da0897d3b116b61c3
GuneetKaur234 <107616806+GuneetKaur234@users.noreply.github.com>
2023-08-12 14:39:47 +0530

    Update Drink_water.py

--- Drink water/Drink_water.py
+++ Drink water/Drink_water.py
@@ -10,16 +10,16 @@ SVSFlag = 11
 spk.Voice
 spk.SetVoice(vcs.Item(speaker_number))
 
-for x in range(0,5):
-    time.sleep(1)
+for x in range(0,6):
+    time.sleep(7200)
     print("hello")
     present_time = (datetime.now())
     x= (present_time + timedelta(seconds=10)).strftime('%H:%M:%S')
-    spk.Speak(f"Hello, its been 10 seconds, its time to Drink water")
+    spk.Speak(f"Hello, its been 2 hours, its time to Drink water")
     notification.notify(
     title = 'Reminder',
     message = 'Drink Water',
-    timeout = 10,
+    timeout = 7200,
     
 )
 
